b1808f7ca6b37ca17927f58f4858358e92429de6
Alden Arce-Romero <aarcero@terpmail.umd.edu>
2026-04-13 14:06:27 -0400

    Started working on simple docstring  for each class as well as added one more class to initilize called Student.

--- automatic_scheduler.py
+++ automatic_scheduler.py
@@ -1,8 +1,14 @@
 #Alden: I started on some core functionality for an automatic scheduler. This is a very basic implementation and can be expanded upon in 
 # the future.
 
+
+
 class Course:
     def __init__(self, name, credits, prerequisites=None, semester_term=None, section=None, time=None, dates=None):
+        """
+        Initializes a Course instance. (P.S I kind of suck at writing docstrings, so if you are pretty good at it
+        feel free to rewrite. We might also need to initilize more classes. So far this is what I have in mind.)
+        """
         if prerequisites is None:
             prerequisites = []
         if dates is None:
@@ -15,8 +21,11 @@ class Course:
         self.time = time
         self.dates = dates
 
-Class Major:
+class Major:
     def __init__(self, name, required_courses=None, elective_courses=None):
+        """
+        Initializes a Major instance.
+        """
         if required_courses is None:
             required_courses = []
         if elective_courses is None:
@@ -24,3 +33,14 @@ Class Major:
         self.name = name
         self.required_courses = required_courses
         self.elective_courses = elective_courses
+
+class Student:
+    def __init__(self, name, major, completed_courses=None):
+        """
+        Initializes a Student instance.
+        """
+        if completed_courses is None:
+            completed_courses = []
+        self.name = name
+        self.major = major
+        self.completed_courses = completed_courses
